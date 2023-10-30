8b35ff50304e14de3ca33559a2c6bdc461b26362
radiaa-devops <123059076+radiaa-devops@users.noreply.github.com>
2023-10-30 20:55:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 9000;
+//const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Anas Assil and Rayyan');
+});
+
+app.listen(PORT, () => {
+  console.log('Running on http');
+});
